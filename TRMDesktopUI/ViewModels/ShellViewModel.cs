c99c0d20041b30b2375225262f2b44f5b791eab9
t3mp0ralflux <t3mp0ralflux@gmail.com>
2019-10-26 21:30:47 -0400

    added WPF application and Caliburn.Micro setup.

---
+++ TRMDesktopUI/ViewModels/ShellViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Caliburn.Micro;
+
+namespace TRMDesktopUI.ViewModels
+{
+    public class ShellViewModel : Conductor<object>
+    {
+    }
+}
---
+++ TRMDesktopUI/bootstrapper.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows;
+using Caliburn.Micro;
+using TRMDesktopUI.ViewModels;
+
+namespace TRMDesktopUI
+{
+    public class Bootstrapper : BootstrapperBase
+    {
+        public Bootstrapper()
+        {
+            Initialize();
+        }
+
+        protected override void OnStartup(object sender, StartupEventArgs e)
+        {
+            DisplayRootViewFor<ShellViewModel>();
+        }
+    }
+}
