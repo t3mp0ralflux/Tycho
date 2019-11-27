1c840f4dd886fc0c2ca0492bd69d7231f0d33a3d
t3mp0ralflux <t3mp0ralflux@gmail.com>
2019-11-27 18:54:30 -0500

    Added Add to Cart method, as well as subtotal, multi-adds to the cart, and ur mum.

--- TRMDesktopUI/ViewModels/SalesViewModel.cs
+++ TRMDesktopUI/ViewModels/SalesViewModel.cs
@@ -1,5 +1,7 @@
 ﻿using Caliburn.Micro;
 using System.ComponentModel;
+using System.Globalization;
+using System.Linq;
 using System.Threading.Tasks;
 using TRMDesktopUI.Library.API;
 using TRMDesktopUI.Library.Models;
@@ -8,10 +10,11 @@ namespace TRMDesktopUI.ViewModels
 {
     public class SalesViewModel : Screen
     {
-        private BindingList<string> _cart;
+        private BindingList<CartItemModel> _cart = new BindingList<CartItemModel>();
         private BindingList<ProductModel> _products;
-        private int _itemQuantity;
+        private int _itemQuantity = 1;
         private IProductEndpoint _productEndpoint;
+        private ProductModel _selectedProduct;
 
         public SalesViewModel(IProductEndpoint productEndpoint)
         {
@@ -40,6 +43,17 @@ namespace TRMDesktopUI.ViewModels
             }
         }
 
+        public ProductModel SelectedProduct
+        {
+            get => _selectedProduct;
+            set
+            {
+                _selectedProduct = value;
+                NotifyOfPropertyChange(() => SelectedProduct);
+                NotifyOfPropertyChange(() => CanAddToCart);
+            }
+        }
+
         public int ItemQuantity
         {
             get => _itemQuantity;
@@ -47,10 +61,11 @@ namespace TRMDesktopUI.ViewModels
             {
                 _itemQuantity = value;
                 NotifyOfPropertyChange(() => ItemQuantity);
+                NotifyOfPropertyChange(() => CanAddToCart);
             }
         }
 
-        public BindingList<string> Cart
+        public BindingList<CartItemModel> Cart
         {
             get => _cart;
             set
@@ -64,8 +79,12 @@ namespace TRMDesktopUI.ViewModels
         {
             get
             {
-                //update this later
-                return "$0.00";
+                decimal subTotal = 0;
+                foreach (var item in Cart)
+                {
+                    subTotal += item.Product.RetailPrice * item.QuantityInCart;
+                }
+                return subTotal.ToString("C");
             }
         }
 
@@ -87,21 +106,31 @@ namespace TRMDesktopUI.ViewModels
             }
         }
 
-        public bool CanAddToCart
-        {
-            get
-            {
-                var output = false;
-
-                // Make sure something is selected
-                // Make sure there is an item quantity
-
-                return output;
-            }
-        }
+        public bool CanAddToCart => ItemQuantity > 0 && SelectedProduct?.QuantityInStock >= ItemQuantity;
 
         public void AddToCart()
         {
+            var existingItem = Cart.FirstOrDefault(x => x.Product == SelectedProduct);
+
+            if (existingItem != null)
+            {
+                existingItem.QuantityInCart += ItemQuantity;
+                // HACK - there's a better way to do this...
+                Cart.Remove(existingItem);
+                Cart.Add(existingItem);
+            }
+            else
+            {
+                var item = new CartItemModel
+                {
+                    Product = SelectedProduct,
+                    QuantityInCart = ItemQuantity
+                };
+                Cart.Add(item);
+            }
+            SelectedProduct.QuantityInStock -= ItemQuantity;
+            ItemQuantity = 1;
+            NotifyOfPropertyChange(() => SubTotal);
         }
 
         public bool CanRemoveFromCart
@@ -119,6 +148,7 @@ namespace TRMDesktopUI.ViewModels
 
         public void RemoveFromCart()
         {
+            NotifyOfPropertyChange(() => SubTotal);
         }
 
         public bool CanCheckOut
